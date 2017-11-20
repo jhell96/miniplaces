88993a8686443229a782d0678ece3543c4df27ad
Josh <jhellerstein96@gmail.com>
2017-11-20 11:49:26 -0500

    eval model

--- model/eval_model.py
+++ model/eval_model.py
@@ -13,7 +13,7 @@ print("Imported modules")
 img_width, img_height = 128, 128
 num_classes = 100
 
-epochs = 20
+epochs = 2
 batch_size = 32
 nb_train_samples = 100000
 nb_validation_samples = 10000
@@ -28,20 +28,20 @@ else:
     input_shape = (img_width, img_height, 3)
 
 model = Xception(include_top=True, weights=None, input_shape=input_shape, pooling=None, classes=num_classes)
-model.load_weights('weights/trained_xception_centered_1.h5')
+model.load_weights('weights/trained_xception_centered_2.h5')
 model.compile("adam", "categorical_crossentropy", ["accuracy", "top_k_categorical_accuracy"])
 print("Compiled model")
 
-datagen = image.ImageDataGenerator(
+test_datagen = image.ImageDataGenerator(
         featurewise_center=True, # set input mean to 0 over the dataset
         samplewise_center=False, # set each sample mean to 0
         featurewise_std_normalization=True, # divide inputs by std of the dataset
         samplewise_std_normalization=False, # divide each input by its std
         zca_whitening=False, # apply ZCA whitening
-        rotation_range=20, # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.2, # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.2, # randomly shift images vertically (fraction of total height)
-        horizontal_flip=True, # randomly flip images
+        rotation_range=0, # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0, # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0, # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False, # randomly flip images
         vertical_flip=False) # randomly flip images
 
 files = []
@@ -56,25 +56,11 @@ imgs = np.array(imgs)
 
 datagen.fit(imgs)
 
-train_generator = datagen.flow_from_directory(
-		train_path,
+test_generator = test_datagen.flow_from_directory(
+		test_path,
 		target_size = (img_width, img_height),
 		batch_size = batch_size,
 		shuffle = True)
 
-validation_generator = datagen.flow_from_directory(
-		val_path,
-		target_size = (img_width, img_height),
-		batch_size = batch_size,
-		shuffle = False)
-
-model.fit_generator(
-		train_generator,
-		steps_per_epoch = nb_train_samples // batch_size, 
-		epochs = epochs,
-		validation_data = validation_generator,
-		validation_steps = nb_validation_samples // batch_size)
-
-model.save_weights('weights/trained_xception_centered_2.h5')
-
-print("Optimization Finished!")
+e = model.evaluate_generator(test_generator)
+print(e)
