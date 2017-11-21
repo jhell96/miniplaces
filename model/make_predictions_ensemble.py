89a37e2024d48cd36dcdfdb3019c6ef28c4f9ccf
Josh <jhellerstein96@gmail.com>
2017-11-21 12:17:14 -0500

    ensembled

--- model/make_predictions_ensemble.py
+++ model/make_predictions_ensemble.py
@@ -1,6 +1,7 @@
 import keras
 from keras.preprocessing import image
 from keras.applications import Xception
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.metrics import top_k_categorical_accuracy
 from keras import backend as K
 import keras_resnet.models
@@ -10,7 +11,8 @@ import os
 from scipy import misc
 print("Imported modules")
 
-img_width, img_height = 128, 128
+img_width_x, img_height_x = 128, 128
+img_width_inc, img_height_inc = 139, 139
 num_classes = 100
 
 epochs = 2
@@ -23,14 +25,21 @@ val_path = '../data/images/val/'
 test_path = '../data/images/test/'
 
 if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
+    input_shape_x = (3, img_width_x, img_height_x)
+    input_shape_inc = (3, img_width_inc, img_height_inc)
 else:
-    input_shape = (img_width, img_height, 3)
+    input_shape_x = (img_width_x, img_height_x, 3)
+    input_shape_inc = (img_width_inc, img_height_inc, 3)
 
-model = Xception(include_top=True, weights=None, input_shape=input_shape, pooling=None, classes=num_classes)
-model.load_weights('weights/trained_xception_centered_2.h5')
-model.compile("adam", "categorical_crossentropy", ["accuracy", "top_k_categorical_accuracy"])
-print("Compiled model")
+model_x = Xception(include_top=True, weights=None, input_shape=input_shape_x, pooling=None, classes=num_classes)
+model_inc = InceptionResNetV2(include_top=True, weights=None, input_shape=input_shape_inc, pooling=None, classes=num_classes)
+
+model_x.load_weights('weights/trained_xception_centered_2.h5')
+model_inc.load_weights('weights/trained_inception_resnet_v2_centered_3.h5')
+
+model_x.compile("adam", "categorical_crossentropy", ["accuracy", "top_k_categorical_accuracy"])
+model_inc.compile("adam", "categorical_crossentropy", ["accuracy", "top_k_categorical_accuracy"])
+print("Compiled models")
 
 test_datagen = image.ImageDataGenerator(
         featurewise_center=True, # set input mean to 0 over the dataset
@@ -65,9 +74,15 @@ test_generator = test_datagen.flow_from_directory(
         shuffle = False,
         class_mode=None)
 
-# results = model.predict(imgs, batch_size=batch_size)
-results = model.predict_generator(test_generator, steps = nb_validation_samples//batch_size)
-top_5 = np.flip(np.argsort(results, axis=1)[:,-5:], 1)
+results_x = model_x.predict_generator(test_generator, steps = nb_validation_samples//batch_size)
+results_inc = model_inc.predict_generator(test_generator, steps = nb_validation_samples//batch_size)
+
+# xception - validation -  loss: 2.994, acc: 0.468, top5 acc: 0.754
+# incep_res - validation - loss: 2.884, acc: 0.444, top5 acc: 0.734
+acc_sum = (0.754 + 0.734)
+results_ensembled = np.average([results_x, results_inc], weights=[0.754/acc_sum, 0.734/(acc_sum)], axis=0)
+
+top_5 = np.flip(np.argsort(results_ensembled, axis=1)[:,-5:], 1)
 
 out = open("output.txt", "a") # output file
 for i in range(len(top_5)):
